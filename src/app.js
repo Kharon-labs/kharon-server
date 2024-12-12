6981036821129412c166f70715ad82106515df13
OkoliEvans <ebounce500@gmail.com>
2024-12-12 03:50:07 +0100

    updated cors with new client url

--- src/app.js
+++ src/app.js
@@ -12,7 +12,28 @@ app.use(helmet());
 let corsOptions = {
     origin: clientUrl,
 }
-app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        clientUrl
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    res.setHeader("Access-Control-Max-Age", 7200);
+
+    next();
+});
+
+// app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
